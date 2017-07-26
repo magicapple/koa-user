0cf096945b9ffb8b77f58a059d9c1f53b3ab0a3c
JinWYP <jinwyp@gmail.com>
2017-07-26 16:29:35 +0800

    update backend views

--- backend/src/app-user/routes/website/page-route.js
+++ backend/src/app-user/routes/website/page-route.js
@@ -8,14 +8,19 @@
 const router = require('koa-router')();
 
 
-async function pageWebIndex(ctx, next) {
+async function pageWebHome(ctx, next) {
     const users = [
               {name : 'Dead Horse'},
               {name : 'Jack'},
               {name : 'Tom'}
           ];
-    await ctx.render('web/index', { page : {title : 'Homepage !'}, users:users });
+    await ctx.render('web/home', { page : {title : 'Homepage !'}, users:users });
 }
+
+async function pageWebLogin(ctx, next) {
+    await ctx.render('web/login', { page : {title : 'Login !'} });
+}
+
 
 async function pageAdminHome(ctx, next) {
 
@@ -25,9 +30,11 @@ async function pageAdminHome(ctx, next) {
 
 
 
-router.redirect('/', '/web/index');
+router.redirect('/', '/web/home');
 
-router.get('/index', pageWebIndex);
+router.get('/login', pageWebLogin);
+router.get('/home', pageWebHome);
+
 
 
 router.get('/admin', pageAdminHome);
