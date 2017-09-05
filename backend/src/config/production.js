ef921dd92ee87d391601513d5823310996bdc7b8
JinWYP <jinwyp@gmail.com>
2017-09-05 10:08:09 +0800

    update

--- backend/src/config/production.js
+++ backend/src/config/production.js
@@ -6,6 +6,16 @@
 const path = require("path")
 
 module.exports = {
-    pathViewTemplate : path.join(__dirname, '../views/dist')
+    pathViewTemplate : path.join(__dirname, '../views/dist'),
+
+    smsNexmo : {
+        API_KEY : '1',
+        API_SECRET : '2'
+    },
+
+    smsAliyun: {
+        AccessKeyId : '3',
+        AccessKeySecret : '4'
+    }
 }
 
