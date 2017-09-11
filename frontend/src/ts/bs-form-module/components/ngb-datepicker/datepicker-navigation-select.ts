348301b5568c9a476d7b7ca58f1aafed05d1714c
JinWYP <jinwyp@gmail.com>
2017-09-11 10:02:30 +0800

    update css

--- frontend/src/ts/bs-form-module/components/ngb-datepicker/datepicker-navigation-select.ts
+++ frontend/src/ts/bs-form-module/components/ngb-datepicker/datepicker-navigation-select.ts
@@ -9,13 +9,14 @@ import {NgbCalendar} from './ngb-calendar'
   changeDetection: ChangeDetectionStrategy.OnPush,
   styles: [`
     select {
-      /* to align with btn-sm */
-      padding: 0.25rem 0.5rem;
-      font-size: 0.875rem;      
-      line-height: 1.25;
-      /* to cancel the custom height set by custom-select */
-      height: inherit;
-      width: 50%;
+        /* to align with btn-sm */
+        padding: 0.25rem 0.5rem;
+        font-size: 0.875rem;      
+        line-height: 1.25;
+        /* to cancel the custom height set by custom-select */
+        height: inherit;
+        width: 50%;
+        background : none;
     }
   `],
   template: `
